a0beab141548941b73c686b6e3df7498bb904180
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2018-02-28 19:52:42 -0500

    Initial

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+    public bool Crash = false;
+    public float MaxSpeed;
+    public float steering;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (!Crash)
+        {
+            float h = -Input.GetAxis("Horizontal");
+            float v = 1;
+
+            Vector2 speed = transform.up * v * MaxSpeed;
+            rb.velocity = speed;
+
+            float direction = Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up));
+            if (direction >= 0.0f)
+            {
+                rb.rotation += h * steering * (rb.velocity.magnitude / 5.0f);
+
+            }
+            else
+            {
+                rb.rotation -= h * steering * (rb.velocity.magnitude / 5.0f);
+
+            }
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Obstacle")
+        {
+            Crash = true;
+        }
+    }
+}
