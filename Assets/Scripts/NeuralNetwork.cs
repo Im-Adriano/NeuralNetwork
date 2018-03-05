d1882730016a71c73764b5614016249f5bdfcb8e
Im-Adriano <35747937+Im-Adriano@users.noreply.github.com>
2018-03-05 15:47:39 -0500

    Update
    More added to NeuralNetwork class

--- Assets/Scripts/NeuralNetwork.cs
+++ Assets/Scripts/NeuralNetwork.cs
@@ -1,20 +1,73 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System;
 
 public class NeuralNetwork : MonoBehaviour {
 
-    int[][] layers;
-    int[][][] weights;
-    int[][] biases;
+    float[][] layers;
+    float[][][] weights;
+    float[][] biases;
 
-	// Use this for initialization
-	void Start () {
-		
+    int hiddenLayers = 1;
+    int inputNum = 5;
+    int hiddenLayerSize = 4;
+    int outputNum = 1;
+    System.Random rand = new System.Random();
+
+    // Use this for initialization
+    void Start () {
+        int[][] layers = new int[hiddenLayers+2][];
+        layers[0] = new int[inputNum];
+        
+
+        for(int i = 1; i <= hiddenLayers; i++)
+        {
+            layers[i] = new int[hiddenLayerSize];
+        }
+
+        layers[layers.Length - 1] = new int[outputNum];
+
+        InitWeights();
+        InitBiases();
+
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void InitWeights()
+    {
+        for (int i = 0; i < hiddenLayers + 1; i++)
+        {
+            weights[i] = new float[layers[i + 1].Length][];
+            for(int j = 0; j < layers[i + 1].Length; j++)
+            {
+                weights[i][j] = new float[layers[i].Length];
+                for(int k = 0; k < layers[i].Length; k++)
+                {
+                    weights[i][j][k] = (float)(rand.NextDouble() * 2) - 1;
+                }
+            }
+        }
+    }
+
+    void InitBiases()
+    {
+        for (int i = 0; i < hiddenLayers + 1; i++)
+        {
+            biases[i] = new float[layers[i+1].Length - 1];
+            for (int j = 0; j < layers[i+1].Length; j++)
+            {
+                biases[i][j] = (float)(rand.NextDouble() - .5);
+            }
+        }
+    }
+
+    public float Compute(float[] inputs)
+    {
+        for (int i = 0; i < layers[0].Length; i++)
+        {
+            layers[0][i] = inputs[i];
+        }
+
+
+        return layers[layers.Length - 1][0];
+    }
+
 }
